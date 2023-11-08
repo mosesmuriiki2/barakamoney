1af8a6903dd44a81daef7d75937c9725065c391f
ngicu moses <ngicumoses@gmai.com>
2023-11-08 16:26:08 +0300

    first commit

---
+++ src/main/java/barakamoney/com/bm/BarakamoneyApplication.java
@@ -0,0 +1,13 @@
+package barakamoney.com.bm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarakamoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarakamoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/barakamoney/com/bm/BarakamoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package barakamoney.com.bm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarakamoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
